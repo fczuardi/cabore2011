864c489b88bf120b3fed6e9ebdf05790a9c7af6b
Fabricio C Zuardi <fabricio@fabricio.org>
2011-09-19 12:30:51 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* Author: 
+
+*/
+var sombra_bg = '<div id="sombra-top"><img src="img/sombra-top.png" /></div><div id="sombra-bottom"><img src="img/sombra-bottom.png" /></div>';
+
+function init(){
+  $('body').append(sombra_bg);
+}
+$(init);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
