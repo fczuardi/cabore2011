ebf7d928d14db6ae9c2fe59285605f0badf7d646
Fabricio C Zuardi <fabricio@fabricio.org>
2011-09-19 15:13:43 -0300

    layout posicionamento dos containers e fundo/sombra/logo preparados para ie6

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@
 var sombra_bg = '<div id="sombra-top"><img src="img/sombra-top.png" /></div><div id="sombra-bottom"><img src="img/sombra-bottom.png" /></div>';
 
 function init(){
-  $('body').append(sombra_bg);
+  var is_ie_6 = $('html').hasClass('ie6');
+  if (!is_ie_6){ $('body').append(sombra_bg); }
 }
 $(init);
 
