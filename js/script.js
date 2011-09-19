84e155ec8b3122ff46b07817a2a44ab394a5b515
Fabricio C Zuardi <fabricio@fabricio.org>
2011-09-19 16:26:38 -0300

    code refactoring

--- js/script.js
+++ js/script.js
@@ -3,18 +3,34 @@
 */
 var sombra_bg = '<div id="sombra-top"><img src="img/sombra-top.png" /></div><div id="sombra-bottom"><img src="img/sombra-bottom.png" /></div>';
 var estrela_ie = '<span style="font-family:Wingdings;font-size:16px;">¬</span>';
+var html_element,
+    body_element;
 
-function init(){
-  var is_ie_6 = $('html').hasClass('ie6');
-  if (!is_ie_6){ 
-    $('body').append(sombra_bg); 
+function addBrowserClasses(){
+  if ($.browser.msie){
+    html_element.addClass('ie');
+  } else{
+    html_element.addClass('notie');
   }
+}
+function tweakContentForIE(){
   if ($.browser.msie){
     $('nav a.vegas').html(estrela_ie+' '+$('nav a.vegas').html()+' '+estrela_ie); 
-    $('html').addClass('ie');
-  } else{
-    $('html').addClass('notie');
   }
+}
+function addShadow(){
+  var is_ie_6 = html_element.hasClass('ie6');
+  if (!is_ie_6){ 
+    body_element.append(sombra_bg); 
+  }
+}
+
+function init(){
+  html_element = $('html');
+  body_element = $('body');
+  addBrowserClasses();
+  tweakContentForIE();
+  addShadow();
 }
 $(init);
 
--- todo.md
+++ todo.md
@@ -1,4 +1,6 @@
-**Todo**
+# Todo
 
 Link para CSS fonts no IE
-http://circuitbomb.com/index.php/2009/09/easy-oft-to-eot-conversion-for-font-face-css3-and-internet-explorer/
+
+ * http://www.fontsquirrel.com/fontface/generator
+ * <strike>http://circuitbomb.com/index.php/2009/09/easy-oft-to-eot-conversion-for-font-face-css3-and-internet-explorer/</strike>
