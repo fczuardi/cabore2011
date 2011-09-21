299bdb76f81ba01c39c3fb15461dc03e44cc6646
Mauricio Zuardi <mauricio.zuardi@gmail.com>
2011-09-21 18:15:52 -0300

    Animação completa

--- js/card-switch-animation.js
+++ js/card-switch-animation.js
@@ -22,9 +22,12 @@ function changeCardOrder(id){
 		//altera a ordem do array
 		var removed_element = card_visual_order.splice(index, 1);
 		card_visual_order.push(removed_element[0]);
+		
 		//atualiza a posição e o z-index
-		updateCardPosition();		
+		// updateCardPosition();
+		updateCardPositionAnimated(750);		
 	} else {
+		//
 		console.log("não preciso trabalhar, senhor");
 	}
 }
@@ -37,6 +40,55 @@ function updateCardPosition(){
 		$('#carta-' + cardId).css("top", card_css_top_values_for_slot[i]);
 		//ajusta o z-index
 		$('#carta-' + cardId).css("z-index", i);
+	}
+}
+
+fila1 = $({});
+
+function updateCardPositionAnimated(time){
+	var default_x = 100;
+	var offset_x = 176 + 10;
+	if(Math.random() > .5){offset_x *= -1}
+	var out_x = default_x + offset_x;
+	//percorre as cartas
+	for(var i=0; i< card_visual_order.length; i++){
+		var cardId = card_visual_order[i];
+		var carta = $('#carta-' + cardId);
+		var meioCaminho = (card_css_top_values_for_slot[i] + carta.position().top)/2;
+		
+		if(i == card_visual_order.length - 1){
+			//anima o top até a metade do caminho
+			carta.animate( {
+				'top': [meioCaminho, 'linear'],
+				'left': [out_x, 'easeInOutQuart']
+				}, time * .4);
+
+			//ajusta o z-index
+			carta.animate( {
+				'z-index':i
+				}, 0);
+
+			//anima o top até a posição final
+			carta.animate( {
+				'top': [card_css_top_values_for_slot[i], 'linear'],
+				'left': [default_x, 'easeInOutQuart']
+				}, time * .6);
+		} else {
+			//anima o top até a metade do caminho
+			carta.animate( {
+				'top': [meioCaminho, 'easeInQuart']
+				}, time * .5);
+
+			//ajusta o z-index
+			carta.animate( {
+				'z-index':i
+				}, 0);
+
+			//anima o top até a posição final
+			carta.animate( {
+				'top': [card_css_top_values_for_slot[i], 'easeOutQuart']
+				}, time * .5);		
+		}
 	}
 }
 
